b445349b24b9dd2461545f920374dff2d71ec21a
Greg Bulmash <YiddishNinja@users.noreply.github.com>
2018-05-24 08:45:35 -0700

    Updating cryptography install line

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo -H pip2 install --upgrade pip
 sudo python -m pip install pip==9.0.3 --upgrade --force-reinstall 
-sudo -H pip install 'cryptography2.2'
+sudo -H pip install 'cryptography<2.2'
 sudo -H pip install flask
 sudo -H pip install flask-ask
